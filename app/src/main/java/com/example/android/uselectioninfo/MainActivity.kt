54edff59554867e19b60914cfd0abac7bce5b5f7
vinchamp77 <vinchamp77@gmail.com>
2021-11-11 09:08:54 +1100

    Initial version that created from default new project "View Model + Fragment"
    Fix all warnings.

---
+++ app/src/main/java/com/example/android/uselectioninfo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.android.uselectioninfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.uselectioninfo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/uselectioninfo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android.uselectioninfo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
