a576a7d80e2dd1c973eba494504eb81f94768e2f
vinchamp77 <vinchamp77@gmail.com>
2021-11-19 14:53:50 +1100

    Implement location service

--- app/src/main/java/com/androidcafe/uselectioninfo/ui/RepresentativesFragment.kt
+++ app/src/main/java/com/androidcafe/uselectioninfo/ui/RepresentativesFragment.kt
@@ -1,22 +1,40 @@
 package com.androidcafe.uselectioninfo.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.androidcafe.uselectioninfo.R
+import com.androidcafe.uselectioninfo.data.Address
 import com.androidcafe.uselectioninfo.databinding.RepresentativesFragmentBinding
 import com.androidcafe.uselectioninfo.viewmodel.RepresentativesViewModel
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import java.util.*
 
 class RepresentativesFragment : BaseFragment() {
 
     private val navController by lazy { findNavController() }
     override val viewModel: RepresentativesViewModel by viewModels()
 
-    companion object {
-        //TODO: Add Constant for Location request
-    }
+    private lateinit var requestLocationPermissionLauncher : ActivityResultLauncher<String>
+    private lateinit var enableLocationSettingLauncher : ActivityResultLauncher<IntentSenderRequest>
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -32,49 +50,157 @@ class RepresentativesFragment : BaseFragment() {
             adapter.submitList(representatives)
         })
 
+        // search button
+        binding.searchButton.setOnClickListener {
+            hideKeyboard()
+            viewModel.onSearchButtonClick()
+        }
+
+        // use my location button
+        registerLocationPermissionsCallback()
+        registerEnableLocationCallback()
+        binding.locationButton.setOnClickListener { requestLocationPermissions()}
+
         return binding.root
+    }
 
+    private fun registerLocationPermissionsCallback() {
 
+        requestLocationPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
 
-        //TODO: Establish button listeners for field and location search
+            if (isGranted) {
+                checkDeviceLocationSettingsAndGetLocation()
+            } else {
+                viewModel.showSnackBarInt.value = R.string.no_location_permission_msg
+            }
+        }
+    }
+
+    private fun registerEnableLocationCallback() {
+        enableLocationSettingLauncher = registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()) { activityResult ->
+                if (activityResult.resultCode == RESULT_OK)
+                    getLocation()
+                else {
+                    viewModel.showSnackBarInt.value = R.string.location_required_error_msg
+                }
+            }
+    }
+
+    private fun requestLocationPermissions() {
+
+        if (isLocationPermissionGranted()) {
+            checkDeviceLocationSettingsAndGetLocation()
+        } else {
+            requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun isLocationPermissionGranted() : Boolean {
+
+        var granted = false
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            granted = true
+        }
+
+        return granted
+    }
+
+    /*
+    *  Uses the Location Client to check the current state of location settings, and gives the user
+    *  the opportunity to turn on location services within our app.
+    */
+    private fun checkDeviceLocationSettingsAndGetLocation(resolve:Boolean = true) {
+
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_LOW_POWER
+        }
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val settingsClient = LocationServices.getSettingsClient(requireActivity())
+        val locationSettingsResponseTask = settingsClient.checkLocationSettings(builder.build())
+
+        locationSettingsResponseTask.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException && resolve){
+                try {
+
+                    val intentSenderRequest = IntentSenderRequest.Builder(exception.resolution).build()
+                    enableLocationSettingLauncher.launch(intentSenderRequest)
+
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    sendEx.printStackTrace()
+                }
+            } else {
+                viewModel.showSnackBarInt.value = R.string.location_required_error_msg
+            }
+        }
+
+        locationSettingsResponseTask.addOnCompleteListener {
+            if ( it.isSuccessful ) {
+                getLocation()
+            }
+        }
+    }
+
+    //permission has been checked before calling this function
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                locationResult.let {
+
+                    val address = geoCodeLocation(it.lastLocation)
+                    viewModel.refreshByCurrentLocation(address)
+
+                    fusedLocationProviderClient.removeLocationUpdates(this)
+                }
+            }
+        }
+
+        val locationRequest = LocationRequest.create()
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+        /* Last location is not very accurate - keeping it here for future reference */
+//        val fusedLocationProviderClient =
+//            LocationServices.getFusedLocationProviderClient(requireContext())
+//        val lastLocationTask = fusedLocationProviderClient.lastLocation
+//
+//        lastLocationTask.addOnCompleteListener(requireActivity()) { task ->
+//
+//            if (task.isSuccessful) {
+//                val taskResult = task.result
+//                taskResult?.run {
+//
+//                    val address = geoCodeLocation(this)
+//                    viewModel.onUseMyLocation(address)
+//                }
+//            }
+//        }
 
     }
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        //TODO: Handle location permission result to get location on permission granted
-//    }
-//
-//    private fun checkLocationPermissions(): Boolean {
-//        return if (isPermissionGranted()) {
-//            true
-//        } else {
-//            //TODO: Request Location permissions
-//            false
-//        }
-//    }
-//
-//    private fun isPermissionGranted() : Boolean {
-//        //TODO: Check if permission is already granted and return (true = granted, false = denied/other)
-//    }
-//
-//    private fun getLocation() {
-//        //TODO: Get location from LocationServices
-//        //TODO: The geoCodeLocation method is a helper function to change the lat/long location to a human readable street address
-//    }
+    private fun geoCodeLocation(location: Location): Address {
+        val geocoder = Geocoder(context, Locale.getDefault())
+        return geocoder.getFromLocation(location.latitude, location.longitude, 1)
+            .map { address ->
+                Address(address.thoroughfare, address.subThoroughfare, address.locality, address.adminArea, address.postalCode)
+            }
+            .first()
+    }
 
-//    private fun geoCodeLocation(location: Location): Address {
-//        val geocoder = Geocoder(context, Locale.getDefault())
-//        return geocoder.getFromLocation(location.latitude, location.longitude, 1)
-//                .map { address ->
-//                    Address(address.thoroughfare, address.subThoroughfare, address.locality, address.adminArea, address.postalCode)
-//                }
-//                .first()
-//    }
-//
-//    private fun hideKeyboard() {
-//        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view!!.windowToken, 0)
-//    }
-
+    private fun hideKeyboard() {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
 }
--- app/src/main/java/com/androidcafe/uselectioninfo/viewmodel/RepresentativesViewModel.kt
+++ app/src/main/java/com/androidcafe/uselectioninfo/viewmodel/RepresentativesViewModel.kt
@@ -53,20 +53,16 @@ class RepresentativesViewModel(app: Application): BaseViewModel(app) {
         return states.value!!.toList()[stateIndex]
     }
 
-    //TODO: Create function to fetch representatives from API from a provided address
+    fun refreshByCurrentLocation(address: Address) {
 
-    /**
-     *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
+        val stateIndex = _states.value?.indexOf(address.state)
+        if (stateIndex != null && stateIndex >= 0) {
+            selectedStateIndex.value = stateIndex!!
+            _address.value = address
+            refreshRepresentatives()
 
-    val (offices, officials) = getRepresentativesDeferred.await()
-    _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
-
-    Note: getRepresentatives in the above code represents the method used to fetch data from the API
-    Note: _representatives in the above code represents the established mutable live data housing representatives
-
-     */
-
-    //TODO: Create function get address from geo location
-
-    //TODO: Create function to get address from individual fields
+        } else {
+            showSnackBarInt.value = R.string.current_location_is_not_us_msg
+        }
+    }
 }
